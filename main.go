539819e5b5a09968d0ae658857b5ae5198a84788
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-08 14:59:07 +0100

    add initial file reading

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	readFile, err := os.Open("test.log")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+	readFile.Close()
 }
