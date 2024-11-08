64d03adce2aa92ec7ac9efdbfd22bf5163102c89
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-08 16:04:13 +0100

    refactor logic into functions

--- main.go
+++ main.go
@@ -12,20 +12,43 @@ type Entry struct {
 	log  string
 }
 
-func main() {
-	var logs []Entry
-	readFile, err := os.Open("test.log")
+func readFile(filename string) ([]string, error) {
+	var lines []string
+	readFile, err := os.Open(filename)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
+	defer readFile.Close()
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
 	for fileScanner.Scan() {
-		var line = strings.SplitN(fileScanner.Text(), "] ", 2)
-		var entry = Entry{
-			time: strings.Trim(line[0], "["),
-			log:  line[1]}
-		logs = append(logs, entry)
+		lines = append(lines, fileScanner.Text())
 	}
-	readFile.Close()
+	if err := fileScanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func parseEntries(lines []string) []Entry {
+	var entries []Entry
+	for _, line := range lines {
+		var parts = strings.SplitN(line, "] ", 2)
+		var entry = Entry{
+			time: parts[0][12:],
+			log:  parts[1]}
+		entries = append(entries, entry)
+	}
+	return entries
+}
+
+func main() {
+	lines, err := readFile("./test.log")
+	if err != nil {
+		fmt.Println("Error parsing file", err)
+	}
+	entries := parseEntries(lines)
+	for _, entry := range entries {
+		fmt.Printf("Time: %v -> %v \n\n", entry.time, entry.log)
+	}
 }
