38e44969fcce6e0cad47dd8f28385a14be025619
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-06 15:30:18 +0100

    wip commit

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"regexp"
 	"strings"
@@ -47,13 +46,60 @@ func parseEntries(lines []string) []Entry {
 	return entries
 }
 
+func Search(needle []rune, hayStack []rune) int {
+	n := len(needle)
+	maxLen := len(hayStack)
+	skipTable := shiftArray(needle)
+
+	position := n - 1
+	for position < maxLen {
+		println("Current position", position)
+		i := n - 1
+		for ; i >= 0 && needle[i] == hayStack[position]; i, position = i-1, position-1 {
+			println("Inner for loop with i: ", i, " and postion: ", position)
+		}
+		if i == -1 {
+			return position + 1
+		}
+		_, exist := skipTable[hayStack[position]]
+		if exist {
+			position = position + skipTable[hayStack[position]]
+		} else {
+			position = position + n
+		}
+	}
+	return -1
+}
+
+func shiftArray(needle []rune) map[rune]int {
+	result := make(map[rune]int)
+	n := len(needle)
+	for i := n - 1; i >= 0; i-- {
+		_, exists := result[needle[i]]
+		if !exists {
+			result[needle[i]] = n - 1 - i
+		}
+	}
+	return result
+}
+
 func main() {
-	lines, err := readFile("./test.log")
+	/*lines, err := readFile("./clean_laravel-2024-11-07.log")
 	if err != nil {
 		fmt.Println("Error parsing file", err)
 	}
 	entries := parseEntries(lines)
 	for _, entry := range entries {
 		fmt.Printf("Time: %v -> %v \n\n", entry.time, entry.log)
-	}
+	}*/
+	needle := make([]rune, 0)
+	needle = append(needle, []rune("Hooligan")...)
+	hayStack := make([]rune, 0)
+	hayStack = append(hayStack, []rune("Hoola-Hoola girls like Hooligans")...)
+	result := Search(needle, hayStack)
+	println(result)
+	/*result := shiftArray(resultx)
+	for i, r := range result {
+		fmt.Printf("Index %c: Numder: %d (Unicode %U)\n", i, r, r)
+	}*/
 }
